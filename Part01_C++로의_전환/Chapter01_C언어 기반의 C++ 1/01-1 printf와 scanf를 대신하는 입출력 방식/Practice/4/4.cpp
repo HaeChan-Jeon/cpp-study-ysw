39214d2b5a0b23b50e57544af88770e9d6e92aa0
HaeChan Jeon <haechan.jeon.memoir@gmail.com>
2025-08-02 14:25:54 +0900

    STUDY: Practice 4

---
+++ Part01_C++로의_전환/Chapter01_C언어 기반의 C++ 1/01-1 printf와 scanf를 대신하는 입출력 방식/Practice/4/4.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+
+int main()
+{
+    int val;
+
+	while (true)
+	{
+        std::cout << "판매 금액을 만원 단위로 입력(-1 to end): ";
+        std::cin >> val;
+
+        if (val == -1)
+        {
+            std::cout << "프로그램을 종료합니다.";
+            return 0;
+        }
+
+        std::cout << "이번 달 급여: " << 50 + val * 0.12 << "만원" << std::endl;
+	}
+
+    return 0;
+}
