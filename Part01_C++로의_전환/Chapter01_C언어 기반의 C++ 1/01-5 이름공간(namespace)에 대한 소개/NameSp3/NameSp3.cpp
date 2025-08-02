f56de37887cb1833688347d0a434d41e60cdccc4
HaeChan Jeon <haechan.jeon.memoir@gmail.com>
2025-08-03 08:01:35 +0900

    STUDY: NameSp3

---
+++ Part01_C++로의_전환/Chapter01_C언어 기반의 C++ 1/01-5 이름공간(namespace)에 대한 소개/NameSp3/NameSp3.cpp
@@ -0,0 +1,40 @@
+﻿#include <iostream>
+
+namespace BestComTmp1
+{
+    void SimpleFunc(void);
+}
+
+namespace BestComTmp1
+{
+    void PrettyFunc(void);
+}
+
+namespace ProgComTmp1
+{
+    void SimpleFunc(void);
+}
+
+int main()
+{
+    BestComTmp1::SimpleFunc();
+
+    return 0;
+}
+
+void BestComTmp1::SimpleFunc(void)
+{
+    std::cout << "BestCom이 정의한 함수" << std::endl;
+    PrettyFunc();
+    ProgComTmp1::SimpleFunc();
+}
+
+void BestComTmp1::PrettyFunc(void)
+{
+    std::cout << "So Pretty!!" << std::endl;
+}
+
+void ProgComTmp1::SimpleFunc(void)
+{
+    std::cout << "ProgComTmp1이 정의한 함수" << std::endl;
+}
