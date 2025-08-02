a083a40fd845f8396983c8509d21d9acee531402
HaeChan Jeon <haechan.jeon.memoir@gmail.com>
2025-08-03 08:12:54 +0900

    STUDY: UsingDcl3

---
+++ Part01_C++로의_전환/Chapter01_C언어 기반의 C++ 1/01-5 이름공간(namespace)에 대한 소개/UsingDcl3/UsingDcl3.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    int num = 20;
+
+    cout << "Hello World!" << endl;
+    cout << "Hello " << "World!" << endl;
+
+    cout << num << ' ' << endl;
+
+    return 0;
+}
