af56e816d3bf74c8f2c2e14114098c5fb3aac28e
HaeChan Jeon <haechan.jeon.memoir@gmail.com>
2025-08-02 14:25:40 +0900

    STUDY: Practice 1

---
+++ Part01_C++로의_전환/Chapter01_C언어 기반의 C++ 1/01-1 printf와 scanf를 대신하는 입출력 방식/Practice/1/1.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+
+int main()
+{
+    int result = 0;
+
+	for (int i = 0; i < 5; i++)
+	{
+		int val;
+
+	    std::cout << i + 1 << "번째 정수 입력: ";
+		std::cin >> val;
+
+		result += val;
+	}
+	
+	std::cout << "합계: " << result;
+	return 0;
+}
