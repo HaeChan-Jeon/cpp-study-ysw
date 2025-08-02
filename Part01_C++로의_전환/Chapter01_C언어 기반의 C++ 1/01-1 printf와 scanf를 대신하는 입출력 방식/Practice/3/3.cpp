d0babdc2e6ee41625a6807d6a1cda43cc4f1e9dc
HaeChan Jeon <haechan.jeon.memoir@gmail.com>
2025-08-02 14:25:52 +0900

    STUDY: Practice 3

---
+++ Part01_C++로의_전환/Chapter01_C언어 기반의 C++ 1/01-1 printf와 scanf를 대신하는 입출력 방식/Practice/3/3.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+
+int main()
+{
+    int val;
+
+    std::cin >> val;
+
+    for (int i = 0; i < 9; i++)
+    {
+        std::cout << val * (i + 1) << std::endl;
+    }
+
+    return 0;
+}
