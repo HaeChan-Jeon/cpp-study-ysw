0815a1e99674ded2edbf603041ea1b881ca1c3fa
HaeChan Jeon <haechan.jeon.memoir@gmail.com>
2025-08-04 21:44:06 +0900

    STUDY: TrueAndFalse

---
+++ Part01_C++로의_전환/Chapter02_C언어 기반의 C++ 2/02-2 새로운 자료형 bool/TrueAndFalse/TrueAndFalse.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int num = 10;
+    int i = 0;
+
+    cout << "True: " << true << endl;
+    cout << "False: " << false << endl;
+
+    while (true)
+    {
+        cout << i++ << ' ';
+
+        if (i > num)
+        {
+            break;
+        }
+    }
+
+    cout << endl;
+
+    cout << "sizeOf 1: " << sizeof(1) << endl;
+    cout << "sizeOf 0: " << sizeof(0) << endl;
+    cout << "sizeOf true: " << sizeof(true) << endl;
+    cout << "sizeOf false: " << sizeof(false) << endl;
+
+    return 0;
+}
