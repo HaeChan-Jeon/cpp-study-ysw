43e9bc769a74f17959aa2d986f813d370ee907d0
HaeChan Jeon <haechan.jeon.memoir@gmail.com>
2025-08-05 21:47:10 +0900

    STUDY: Practice 1

---
+++ Part01_C++로의_전환/Chapter02_C언어 기반의 C++ 2/02-4 참조자(Reference)와 함수/Practice/1/1.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+
+using namespace std;
+
+void Add(int &ref);
+
+void Change(int& ref);
+
+int main()
+{
+    int num = 10;
+    std::cout << num << endl;
+
+    Add(num);
+    std::cout << num << endl;
+
+    Change(num);
+    std::cout << num << endl;
+
+    return 0;
+}
+
+void Add(int& ref)
+{
+    ref++;
+}
+
+void Change(int& ref)
+{
+    ref = -ref;
+}
