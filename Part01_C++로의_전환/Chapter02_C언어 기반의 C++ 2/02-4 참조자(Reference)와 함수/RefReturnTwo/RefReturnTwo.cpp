b18ff155490acd4900d32b67524c44ddbcad1e64
HaeChan Jeon <haechan.jeon.memoir@gmail.com>
2025-08-05 21:46:23 +0900

    STUDY: RefReturnTwo

---
+++ Part01_C++로의_전환/Chapter02_C언어 기반의 C++ 2/02-4 참조자(Reference)와 함수/RefReturnTwo/RefReturnTwo.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int RefRetFuncOne(int& ref)
+{
+    ref++;
+    return ref;
+}
+
+int main()
+{
+    int num1 = 1;
+    int num2 = RefRetFuncOne(num1);
+
+    num1++;
+    num2+=100;
+
+    std::cout << "num1: " << num1 << endl;
+    std::cout << "num2: " << num2 << endl;
+
+    return 0;
+}
