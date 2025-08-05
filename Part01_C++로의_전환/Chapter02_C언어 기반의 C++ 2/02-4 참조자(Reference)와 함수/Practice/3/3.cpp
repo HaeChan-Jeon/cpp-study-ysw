c6bd68bc807fe0e5b24026697a0c14328ad09627
HaeChan Jeon <haechan.jeon.memoir@gmail.com>
2025-08-05 21:47:19 +0900

    STUDY: Practice 3

---
+++ Part01_C++로의_전환/Chapter02_C언어 기반의 C++ 2/02-4 참조자(Reference)와 함수/Practice/3/3.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+
+using namespace std;
+
+void SwapPointer(int* ptr1, int* ptr2);
+
+int main()
+{
+    int num1 = 5;
+    int* ptr1 = &num1;
+    int num2 = 10;
+    int* ptr2 = &num2;
+
+    std::cout << "ptr1: " << *ptr1 << endl;
+    std::cout << "ptr2: " << *ptr2 << endl;
+
+    SwapPointer(ptr1, ptr2);
+
+    std::cout << "ptr1: " << *ptr1 << endl;
+    std::cout << "ptr2: " << *ptr2 << endl;
+
+    return 0;
+}
+
+void SwapPointer(int* ptr1, int* ptr2)
+{
+    int temp = *ptr1;
+    *ptr1 = *ptr2;
+    *ptr2 = temp;
+}
