d386ac0ffd1afc7158596651ad05a41e0c52c423
HaeChan Jeon <haechan.jeon.memoir@gmail.com>
2025-08-05 21:46:47 +0900

    STUDY: NewDelete

---
+++ Part01_C++로의_전환/Chapter02_C언어 기반의 C++ 2/02-5 malloc & free를 대신하는 new & delete/NewDelete/NewDelete.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+#include <string.h>
+
+using namespace std;
+
+char* MakeStrAdr(int len)
+{
+    char* str = new char[len];
+    
+    return str;
+}
+
+int main()
+{
+    char* str = MakeStrAdr(20);
+    
+    strcpy_s(str, 20, "I am so happy~");
+    
+    cout << str << endl;
+    
+    delete []str;
+
+    return 0;
+}
