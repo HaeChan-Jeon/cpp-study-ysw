7849273be771eb272877176e34c1eb253b56552b
HaeChan Jeon <haechan.jeon.memoir@gmail.com>
2025-08-07 22:35:44 +0900

    STUDY: Practice 03-2-2

---
+++ Part02_객체지향의_도입/Chapter 03 클래스의 기본/03-2 클래스(Class)와 객체(Object)/Practice/03-2-2/03-2-2.cpp
@@ -0,0 +1,40 @@
+﻿#include <iostream>
+
+using namespace std;
+
+namespace PRT_CONST
+{
+    string emptyStr = "";
+}
+
+class Printer
+{
+private:
+    string setStr = PRT_CONST::emptyStr;
+
+public:
+    void SetString(string str);
+    void ShowString();
+};
+
+void Printer::SetString(string str)
+{
+    setStr = str;
+}
+
+void Printer::ShowString()
+{
+    cout << setStr << endl;
+}
+
+int main()
+{
+    Printer pnt;
+    pnt.SetString("Hello world!");
+    pnt.ShowString();
+
+    pnt.SetString("I Love C++");
+    pnt.ShowString();
+
+    return 0;
+}
