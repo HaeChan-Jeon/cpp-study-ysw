49f63e0f703fc935e0387590ebbe9d639177d820
HaeChan Jeon <haechan.jeon.memoir@gmail.com>
2025-08-10 11:04:34 +0900

    STUDY: ObjArr

---
+++ Part02_객체지향의_도입/Chapter 04 클래스의 완성/04-4 클래스와 배열 그리고 this 포인터/ObjArr/ObjArr.cpp
@@ -0,0 +1,58 @@
+﻿#include <iostream>
+#include <cstring>
+
+using namespace std;
+
+class Person
+{
+private:
+    string name;
+    int age;
+
+public:
+    Person(string myname, int myage) : name(myname), age(myage)
+    {
+    }
+    Person()
+    {
+        name = "";
+        age = 0;
+        cout << "called Person()" << endl;
+    }
+    void SerPersonInfo(string myname, int myage)
+    {
+        name = myname;
+        age = myage;
+    }
+    void ShowPersonInfo() const
+    {
+        cout << "이름: " << name << ", ";
+        cout << "나이: " << age << endl;
+    }
+    ~Person()
+    {
+        cout << "called destructor!" << endl;
+    }
+};
+
+int main()
+{
+    Person parr[3];
+    string name;
+    int age;
+
+    for (int i = 0; i < 3; ++i)
+    {
+        cout << "이름: ";
+        cin >> name;
+        cout << "나이: ";
+        cin >> age;
+        parr[i].SerPersonInfo(name, age);
+    }
+
+    parr[0].ShowPersonInfo();
+    parr[1].ShowPersonInfo();
+    parr[2].ShowPersonInfo();
+
+    return 0;
+}
