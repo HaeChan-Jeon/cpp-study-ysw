ba5ae4eee35cba93742c0fd6a80f125a1e8fb582
HaeChan Jeon <haechan.jeon.memoir@gmail.com>
2025-08-11 20:56:06 +0900

    STUDY: Practice 05-1

---
+++ Part02_객체지향의_도입/Chapter 05 복사 생성자(Copy Constructor)/05-2 '깊은 복사'와 '얕은 복사'/Practice/05-1/05-1.cpp
@@ -0,0 +1,72 @@
+﻿#include <iostream>
+#include <string>
+
+using namespace std;
+
+namespace COMP_POS
+{
+    enum
+    {
+        CLEARK,
+        SENNIOR,
+        ASSIST,
+        MANAGER
+    };
+}
+
+class NameCard
+{
+private:
+    string name;
+    string companyName;
+    string phoneNumber;
+    int position;
+
+    const string PositionToName(int position)
+    {
+        string name = "";
+
+        switch (position)
+        {
+        case COMP_POS::CLEARK:
+            name = "사원";
+            break;
+        case COMP_POS::SENNIOR:
+            name = "주임";
+            break;
+        case COMP_POS::ASSIST:
+            name = "대리";
+            break;
+        case COMP_POS::MANAGER:
+            name = "과장";
+            break;
+        }
+
+        return name;
+    }
+
+public:
+    NameCard(string name, string companyName, string phoneNumber, int positon)
+        : name(name), companyName(companyName), phoneNumber(phoneNumber), position(positon)
+    {
+    }
+
+    const void ShowNameCardInfo()
+    {
+        cout << "이름: " << name << endl;
+        cout << "회사: " << companyName << endl;
+        cout << "전화번호: " << phoneNumber << endl;
+        cout << "직급: " << PositionToName(position) << endl << endl;
+    }
+};
+
+int main()
+{
+    NameCard manClerk("Lee", "ABCEng", "010-1111-2222", COMP_POS::CLEARK);
+    NameCard copy1(manClerk);
+    NameCard manSENIOR("Hong", "OrangeEng", "010-3333-4444", COMP_POS::MANAGER);
+    NameCard copy2(manSENIOR);
+    copy1.ShowNameCardInfo();
+    copy2.ShowNameCardInfo();
+    return 0;
+}
