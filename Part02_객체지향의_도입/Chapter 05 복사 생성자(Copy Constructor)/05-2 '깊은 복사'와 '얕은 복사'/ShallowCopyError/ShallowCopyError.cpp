9e9623bc9ae685b1b89aaca87a9068fa5df6ca1a
HaeChan Jeon <haechan.jeon.memoir@gmail.com>
2025-08-11 20:56:14 +0900

    STUDY: ShallowCopyError

---
+++ Part02_객체지향의_도입/Chapter 05 복사 생성자(Copy Constructor)/05-2 '깊은 복사'와 '얕은 복사'/ShallowCopyError/ShallowCopyError.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Person
+{
+private:
+    string name;
+    int age;
+
+public:
+    Person(const string& myname, int myage)
+        : name(myname), age(myage) {}
+
+    void ShowPersonInfo() const
+    {
+        cout << "이름: " << name << endl;
+        cout << "나이: " << age << endl;
+    }
+    ~Person()
+    {
+        cout << "called destructor for " << name << endl;
+    }
+};
+
+int main()
+{
+    Person man1("Lee dong woo", 29);
+    Person man2 = man1;
+    man1.ShowPersonInfo();
+    man2.ShowPersonInfo();
+    return 0;
+}
