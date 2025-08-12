b4d110690c7083548d2f64455df144c7faa2736a
HaeChan Jeon <haechan.jeon.memoir@gmail.com>
2025-08-12 22:14:52 +0900

    STUDY: MyFriendClass

---
+++ Part02_객체지향의_도입/Chapter 06 friend와 static 그리고 const/06-2 클래스와 함수에 대한 friend 선언/MyFriendClass/MyFriendClass.cpp
@@ -0,0 +1,45 @@
+﻿#include <iostream>
+
+using namespace std;
+
+class Girl;
+
+class Boy
+{
+private:
+    int height;
+    friend class Girl;
+
+public:
+    Boy(int len) : height(len)
+    { }
+    void ShowYourFriendInfo(Girl& frn);
+};
+
+class Girl
+{
+private:
+    string phNum;
+public:
+    Girl(string num) : phNum(num)
+    { }
+    void ShowYourFriendInfo(Boy& frn);
+    friend class Boy;
+};
+
+void Boy::ShowYourFriendInfo(Girl& frn)
+{
+    cout << "Her Phone number: " << frn.phNum << endl;
+}
+
+void Girl::ShowYourFriendInfo(Boy& frn)
+{
+    cout << "His Phone number: " << frn.height << endl;
+}
+int main()
+{
+    Boy boy(170);
+    Girl girl("010-1234-5678");
+    boy.ShowYourFriendInfo(girl);
+    girl.ShowYourFriendInfo(boy);
+}
