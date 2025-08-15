f21a5faa3ae4b7c375a2fa8c68cf80b7e3bec355
HaeChan Jeon <haechan.jeon.memoir@gmail.com>
2025-08-15 19:07:00 +0900

    STUDY: Practice 1

---
+++ Part02_객체지향의_도입/Chapter 07 상속(Inheritance)의 이해/07-4 상속을 위한 조건/Practice/1/1.cpp
@@ -0,0 +1,38 @@
+﻿#include <iostream>
+
+using namespace std;
+
+class Rectangle
+{
+private:
+	int a;
+	int b;
+public:
+	Rectangle(int num)
+		: a(num), b(num)
+	{ }
+	Rectangle(int num1, int num2)
+		: a(num1), b(num2)
+	{ }
+	void ShowAreaInfo() const
+	{
+		cout << "면적: " << a * b << endl;
+	}
+};
+
+class Square : public Rectangle
+{
+public:
+	Square(int num) : Rectangle(num)
+	{ }
+};
+
+int main()
+{
+	Rectangle rec(4, 3);
+	rec.ShowAreaInfo();
+
+	Square sqr(7);
+	sqr.ShowAreaInfo();
+	return 0;
+}
