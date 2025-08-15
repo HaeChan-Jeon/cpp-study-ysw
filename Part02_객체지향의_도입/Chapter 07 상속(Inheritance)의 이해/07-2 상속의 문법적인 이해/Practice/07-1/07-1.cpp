db9aae2256ad41becf952ed5d5d1db2e72f69624
HaeChan Jeon <haechan.jeon.memoir@gmail.com>
2025-08-15 19:06:49 +0900

    STUDY: Practice 07-1

---
+++ Part02_객체지향의_도입/Chapter 07 상속(Inheritance)의 이해/07-2 상속의 문법적인 이해/Practice/07-1/07-1.cpp
@@ -0,0 +1,56 @@
+﻿#include <iostream>
+
+using namespace std;
+
+class Car
+{
+private:
+    int gasolineGauge;
+public:
+    Car(int gas)
+        : gasolineGauge(gas)
+    { }
+    int GetGasGauge()
+    {
+        return gasolineGauge;
+    }
+};
+
+class HybridCar : public Car
+{
+private:
+    int electricGauge;
+public:
+    HybridCar(int gas, int electic)
+        : Car(gas), electricGauge(electic)
+    { }
+    int GetElecGauge()
+    {
+        return electricGauge;
+    }
+};
+
+class HybridWaterCar : public HybridCar
+{
+private:
+    int waterGauge;
+public:
+    HybridWaterCar(int gas, int electric, int water)
+        : HybridCar(gas, electric), waterGauge(water)
+    { }
+    void ShowCurrentGauge()
+    {
+        cout << "잔여 가솔린: " << GetGasGauge() << endl;
+        cout << "잔여 전기량: " << GetElecGauge() << endl;
+        cout << "잔여 워터량: " << waterGauge << endl;
+    }
+};
+
+int main()
+{
+    HybridWaterCar car(3, 4, 5);
+
+    car.ShowCurrentGauge();
+
+    return 0;
+}
