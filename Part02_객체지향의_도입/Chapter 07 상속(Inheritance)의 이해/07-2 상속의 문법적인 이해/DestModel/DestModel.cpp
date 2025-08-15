602c8ec00c3a46bfcacacf0fd255625e0f3f3a4d
HaeChan Jeon <haechan.jeon.memoir@gmail.com>
2025-08-15 19:06:37 +0900

    STUDY: DestModel

---
+++ Part02_객체지향의_도입/Chapter 07 상속(Inheritance)의 이해/07-2 상속의 문법적인 이해/DestModel/DestModel.cpp
@@ -0,0 +1,43 @@
+﻿#include <iostream>
+
+using namespace std;
+
+class Person
+{
+private:
+    string name;
+public:
+    Person(string myname)
+        : name(myname)
+    { }
+    void WhatYourName() const
+    {
+        cout << "My name is " << name << endl;
+    }
+};
+
+class UnivStudent : public Person
+{
+private:
+    string major;
+public:
+    UnivStudent(string myname, string mymajor)
+        : Person(myname), major(mymajor)
+    { }
+    void WhoAreYou() const
+    {
+        WhatYourName();
+        cout << "My major is " << major << endl << endl;
+    }
+};
+
+int main()
+{
+    UnivStudent st1("Kim", "Mathematics");
+    st1.WhoAreYou();
+
+    UnivStudent st2("Hong", "Physics");
+    st2.WhoAreYou();
+
+    return 0;
+}
