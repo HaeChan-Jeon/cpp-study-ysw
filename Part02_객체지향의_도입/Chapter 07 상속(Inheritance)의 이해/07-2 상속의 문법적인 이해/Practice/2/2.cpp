b6880dcc9ad5bc63c9f31358eb15fd676439db48
HaeChan Jeon <haechan.jeon.memoir@gmail.com>
2025-08-15 19:06:54 +0900

    STUDY: Practice 2

---
+++ Part02_객체지향의_도입/Chapter 07 상속(Inheritance)의 이해/07-2 상속의 문법적인 이해/Practice/2/2.cpp
@@ -0,0 +1,45 @@
+﻿#include <iostream>
+
+using namespace std;
+
+class MyFriendInfo
+{
+private:
+    string name;
+    int age;
+public:
+    MyFriendInfo(string fname, int fage)
+        : name(fname), age(fage)
+    { }
+    void ShowMyFriendInfo()
+    {
+        cout << "이름: " << name << endl;
+        cout << "나이: " << age << endl;
+    }
+};
+
+class MyFriendDetailInfo : public MyFriendInfo
+{
+private:
+    string addr;
+    string phone;
+public:
+    MyFriendDetailInfo(string fname, int fage, string faddr, string fphone)
+        : MyFriendInfo(fname, fage), addr(faddr), phone(fphone)
+    { }
+    void ShowMyFriendDetailInfo()
+    {
+        ShowMyFriendInfo();
+        cout << "주소: " << addr << endl;
+        cout << "전화: " << phone << endl << endl;
+    }
+};
+
+int main()
+{
+    MyFriendDetailInfo f1("park", 27, "suri", "000");
+    MyFriendDetailInfo f2("jeon", 37, "surir", "010");
+    f1.ShowMyFriendDetailInfo();
+    f2.ShowMyFriendDetailInfo();
+    return 0;
+}
