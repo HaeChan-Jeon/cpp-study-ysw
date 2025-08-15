d07378def2472a77e7b25a1f3e54f66934d9c56c
HaeChan Jeon <haechan.jeon.memoir@gmail.com>
2025-08-15 19:07:26 +0900

    STUDY: UnivStudentInheri

---
+++ Part02_객체지향의_도입/Chapter 07 상속(Inheritance)의 이해/07-2 상속의 문법적인 이해/UnivStudentInheri/UnivStudentInheri.cpp
@@ -0,0 +1,49 @@
+﻿#include <iostream>
+
+using namespace std;
+
+class Person
+{
+private:
+    int age;
+    string name;
+public:
+    Person(int myage, string myname)
+        : age(myage), name(myname)
+    { }
+    void WhatYourName() const
+    {
+        cout << "My name is " << name << endl;
+    }
+    void HowOldAreYou() const
+    {
+        cout << "I'm " << age << " years old" << endl;
+    }
+};
+
+class UnivStudent : public Person
+{
+private:
+    string major;
+public:
+    UnivStudent(string myname, int myage, string mymajor)
+        : Person(myage, myname), major(mymajor)
+    { }
+    void WhoAreYou() const
+    {
+        WhatYourName();
+        HowOldAreYou();
+        cout << "My major is " << major << endl << endl;
+    }
+};
+
+int main()
+{
+    UnivStudent ustd1("Lee", 22, "Computer eng.");
+    ustd1.WhoAreYou();
+
+    UnivStudent ustd2("Yoon", 21, "Electronic eng.");
+    ustd2.WhoAreYou();
+
+    return 0;
+}
