4827b82a60ecbebdff3bf3574eacfed73239b808
HaeChan-Jeon <haechan.jeon.memoir@gmail.com>
2025-08-19 07:51:46 +0900

    STUDY: VirtualDestructor

---
+++ Part02_객체지향의_도입/Chapter 08 상속과 다형성/08-3 가상 소멸자와 참조자의 참조 가능성/VirtualDestructor/VirtualDestructor.cpp
@@ -0,0 +1,37 @@
+﻿#include <iostream>
+
+using namespace std;
+
+class First
+{
+private:
+    string strOne;
+public:
+    First(string str) : strOne(str)
+    { }
+    ~First()
+    {
+        cout << "~First()" << endl;
+    }
+};
+
+class Second : public First
+{
+private:
+    string strTwo;
+public:
+    Second(string str1, string str2)
+        : First(str1), strTwo(str2)
+    { }
+    ~Second()
+    {
+        cout << "~Second()" << endl;
+    }
+};
+
+int main(void)
+{
+    First* ptr = new Second("Simple", "Complex");
+    delete ptr;
+    return 0;
+}
