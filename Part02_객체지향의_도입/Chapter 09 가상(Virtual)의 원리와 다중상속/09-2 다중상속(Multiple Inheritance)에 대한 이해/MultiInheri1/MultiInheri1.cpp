583a25c5414a4aaba9ab73685c7c55717a0a78c9
HaeChan-Jeon <haechan.jeon.memoir@gmail.com>
2025-08-19 07:52:25 +0900

    STUDY: MultiInheri1

---
+++ Part02_객체지향의_도입/Chapter 09 가상(Virtual)의 원리와 다중상속/09-2 다중상속(Multiple Inheritance)에 대한 이해/MultiInheri1/MultiInheri1.cpp
@@ -0,0 +1,39 @@
+﻿#include <iostream>
+
+using namespace std;
+
+class BaseOne
+{
+public:
+    void SimpleFuncOne()
+    {
+        cout << "BaseOne" << endl;
+    }
+};
+
+class BaseTwo
+{
+public:
+    void SimpleFuncTwo()
+    {
+        cout << "BaseTwo" << endl;
+    }
+};
+
+class MultiDerived : public BaseOne, public BaseTwo
+{
+public:
+    void ComplexFunc()
+    {
+        SimpleFuncOne();
+        SimpleFuncTwo();
+    }
+};
+
+int main(void)
+{
+    MultiDerived mdr;
+    mdr.ComplexFunc();
+
+    return 0;
+}
