6059b519579faa74b2b8ec6f72ed26318ee72d24
HaeChan-Jeon <haechan.jeon.memoir@gmail.com>
2025-08-20 07:40:06 +0900

    STUDY: PostOpndOverloading

---
+++ Part02_객체지향의_도입/Chapter 10 연산자 오버로딩 1/10-2 단항 연산자의 오버로딩/PostOpndOverloading/PostOpndOverloading.cpp
@@ -0,0 +1,61 @@
+﻿#include <iostream>
+
+using namespace std;
+
+class Point
+{
+private:
+    int xpos, ypos;
+public:
+    Point(int x = 0, int y = 0) : xpos(x), ypos(y) { }
+    void ShowPosition() const
+    {
+        cout << '[' << xpos << ", " << ypos << ']' << endl;
+    }
+    Point& operator++()
+    {
+        xpos += 1;
+        ypos += 1;
+        return *this;
+    }
+    Point operator++(int)
+    {
+        const Point retobj(xpos, ypos);
+        xpos += 1;
+        ypos += 1;
+        return retobj;
+    }
+    friend Point& operator--(Point& ref);
+    friend Point operator--(Point& ref, int);
+};
+
+Point& operator--(Point& ref)
+{
+    ref.xpos -= 1;
+    ref.ypos -= 1;
+    return ref;
+}
+
+Point operator--(Point& ref, int)
+{
+    const Point retobj(ref);
+    ref.xpos -= 1;
+    ref.ypos -= 1;
+    return retobj;
+}
+
+int main(void)
+{
+    Point pos(3, 5);
+    Point cpy;
+
+    cpy = pos--;
+    cpy.ShowPosition();
+    pos.ShowPosition();
+
+    cpy = pos++;
+    cpy.ShowPosition();
+    pos.ShowPosition();
+
+    return 0;
+}
